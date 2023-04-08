c7094439ba9d5770487425bcabd1452f149a9566
DamienB2 <100684978+DamienB2@users.noreply.github.com>
2023-04-08 21:19:58 +0200

    PokeBar Pokedex v1

--- app/src/main/java/com/example/pokebar/MainActivity.java
+++ app/src/main/java/com/example/pokebar/MainActivity.java
@@ -1,42 +1,41 @@
 package com.example.pokebar;
 
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.annotation.MainThread;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.PopupWindow;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.FirebaseAuth;
-import com.journeyapps.barcodescanner.CaptureActivity;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -44,6 +43,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
+    LinearLayout pokedexLayout;
 
 
     @Override
@@ -70,8 +70,114 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         navigationView.setCheckedItem(R.id.nav_home);
 
 
+        //this part is for the pokedex
+        pokedexLayout = findViewById(R.id.PokedexContainer);
+        final String[] pokemonName = new String[1];
+        final String[] pokemonType = new String[1];
+
+
+        //for the moment, show all 150 pokemons
+        for (int i = 1; i <= 150; i++) {
+            RequestQueue queue = Volley.newRequestQueue(this);
+
+            String url = "https://pokeapi.co/api/v2/pokemon/"+i;
+
+            // Request a string response from the provided URL.
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                JSONObject jObject = new JSONObject(response);
+
+                                JSONArray types = jObject.getJSONArray("types");
+                                JSONObject entry = types.getJSONObject(0);
+                                JSONObject type = entry.getJSONObject("type");
+                                String requestType = type.getString("name");
+                                pokemonType[0] = requestType;
+
+
+                                //get the pokemon name
+                                String requestName = jObject.getString("name");
+                                pokemonName[0] = requestName;
+
+
+                                //get the pokemon image link
+                                JSONObject PokemonSprites = jObject.getJSONObject("sprites");
+                                JSONObject SpritesOther = PokemonSprites.getJSONObject("other");
+                                JSONObject SpritesOfficial = SpritesOther.getJSONObject("official-artwork");
+                                String ImageUrl = SpritesOfficial.getString("front_default");
+
+
+                                //get the pokemon id
+                                String requestId = jObject.getString("id");
+
+                                addPokemon(pokemonName,pokemonType, ImageUrl,requestId);
+
+
+                            }catch(Exception e) {
+                                System.out.println("Unable to get request result");
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                }
+
+            });
+
+            // Add the request to the RequestQueue.
+            queue.add(stringRequest);
+
+        }
     }
 
+
+    //adding each pokemon to the pokedex.
+    private void addPokemon(String[] name, String[] type, String imageUrl, String id) {
+        View view = getLayoutInflater().inflate(R.layout.pokemon_card, null);
+
+        TextView pokemonNom = view.findViewById(R.id.textViewPokemonName);
+        TextView pokemonType = view.findViewById(R.id.textViewPokemonType);
+        TextView pokemonId = view.findViewById(R.id.textViewPokemonId);
+        ImageView imageView = view.findViewById(R.id.imageViewPokemon);
+
+        pokemonNom.setText("Name: "+name[0]);
+        pokemonType.setText("Type: "+type[0]);
+        pokemonId.setText("Id: "+id);
+
+
+        //this part place the image in the imageView with the link
+        int maxWidth = 1000;
+        int maxHeight = 1000;
+        ImageRequest imageRequest = new ImageRequest(
+                imageUrl,
+                new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap response) {
+                        imageView.setImageBitmap(response);
+                    }
+                },
+                maxWidth,
+                maxHeight,
+                ImageView.ScaleType.CENTER,
+                Bitmap.Config.ARGB_8888,
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Error loading image", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(imageRequest);
+
+
+
+        pokedexLayout.addView(view);
+    }
 
 
     //PopUp Window code
@@ -141,7 +247,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
 
-
     public void ScanCode(View view) {
         ScanOptions options = new ScanOptions();
         options.setPrompt("volume up to set flash on");
@@ -157,4 +262,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             QrText.setText("Vous avez rajouté "+result.getContents()+" à votre Pokédex !");
         }
     });
+
+
+
+
+
+
+
+
 }
